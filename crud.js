bfe41b6b920985b9ef6663e4ac0bf35ff141566c
mtavidal <119874263+mtavidal@users.noreply.github.com>
2023-03-24 15:30:32 -0300

    Conclui as chamadas para a API

--- crud.js
+++ crud.js
@@ -1,6 +1,38 @@
 
-export async function listarModelos(vehicleType, brandId, modelId, yearId){
-    let endpoint = `https://parallelum.com.br/fipe/api/v2/${vehicleType}/brands/${brandId}/models/${modelId}/years/${yearId}`
+export async function consultarCarro(tipo, codigoMarca, codigoModelo, ano){
+    let endpoint = `https://parallelum.com.br/fipe/api/v1/${tipo}/marcas/${codigoMarca}/modelos/${codigoModelo}/anos/${ano}`
+    try {
+        let response = await fetch (endpoint);
+        let data = await response.json();
+        return data;
+    } catch (error) {
+        console.log(`Error: ${error}`)
+    }   
+}
+export async function listarMarcas(tipo){
+    let endpoint = `https://parallelum.com.br/fipe/api/v1/${tipo}/marcas`
+    try {
+        let response = await fetch (endpoint);
+        let data = await response.json();
+        return data;
+    } catch (error) {
+        console.log(`Error: ${error}`)
+    }   
+}
+
+export async function listarModelos(tipo, codigoMarca){
+    let endpoint = `https://parallelum.com.br/fipe/api/v1/${tipo}/marcas/${codigoMarca}/modelos`
+    try {
+        let response = await fetch (endpoint);
+        let data = await response.json();
+        return data;
+    } catch (error) {
+        console.log(`Error: ${error}`)
+    }   
+}
+
+export async function listarAnos(tipo, codigoMarca, codigoModelo){
+    let endpoint = `https://parallelum.com.br/fipe/api/v1/${tipo}/marcas/${codigoMarca}/modelos/${codigoModelo}/anos`
     try {
         let response = await fetch (endpoint);
         let data = await response.json();
--- index.js
+++ index.js
@@ -1,14 +1,60 @@
-import {listarModelos} from "./crud.js";
+import {consultarCarro, listarMarcas, listarModelos, listarAnos} from "./crud.js";
 
 let form = document.getElementById('formCampo');
 
+const buscarMarcaPorTipo= document.getElementById("tipo");
+buscarMarcaPorTipo.onchange = async ()  => {
+    let tipo = buscarMarcaPorTipo.value;
+    let marcas = await listarMarcas(tipo);
+    let selectMarca = document.getElementById("marca");
+    selectMarca.style.display = "flex";
+    selectMarca.innerHTML = `<option value="" disabled selected>selecione</option>`;
+    for (let index = 0; index < marcas.length; index++) {
+        selectMarca.innerHTML += ` 
+            <option value="${marcas[index].codigo}">${marcas[index].nome}</option>
+        `
+    }
+}
+
+const buscarModeloPorMarca= document.getElementById("marca");
+buscarModeloPorMarca.onchange = async ()  => {
+    let tipo = buscarMarcaPorTipo.value;
+    let marca = buscarModeloPorMarca.value;
+    let modelos = await listarModelos(tipo, marca);
+    let selectModelo = document.getElementById("modelo");
+    selectModelo.style.display = "flex";
+    selectModelo.innerHTML = `<option value="" disabled selected>selecione</option>`;
+    for (let index = 0; index < modelos.modelos.length; index++) {
+        selectModelo.innerHTML += ` 
+            <option value="${modelos.modelos[index].codigo}">${modelos.modelos[index].nome}</option>
+        `
+    }
+}
+
+const buscarAnoPorModelo= document.getElementById("modelo");
+buscarAnoPorModelo.onchange = async ()  => {
+    let tipo = buscarMarcaPorTipo.value;
+    let marca = buscarModeloPorMarca.value;
+    let modelo = buscarAnoPorModelo.value;
+    let anos = await listarAnos(tipo, marca, modelo);
+    let selectAno = document.getElementById("ano");
+    selectAno.style.display = "flex";
+    selectAno.innerHTML = `<option value="" disabled selected>selecione</option>`;
+    for (let index = 0; index < anos.length; index++) {
+        selectAno.innerHTML += ` 
+            <option value="${anos[index].codigo}">${anos[index].nome}</option>
+        `
+    }
+}
+
+const buscarCarroPorAno = document.getElementById("ano");
 form.addEventListener('submit', async event => {
     event.preventDefault();
-    let vehicleType = document.getElementById('tipo').value;
-    let brandId = document.getElementById('marca').value;
-    let modelId = document.getElementById('modelo').value;
-    let yearId = document.getElementById('data').value;
-    let dadosConsulta = await listarModelos(vehicleType, brandId, modelId, yearId);
+    let tipo = buscarMarcaPorTipo.value;
+    let marca = buscarModeloPorMarca.value;
+    let modelo = buscarAnoPorModelo.value;
+    let ano = buscarCarroPorAno.value;
+    let dadosConsulta = await consultarCarro(tipo, marca, modelo, ano);
     console.log(dadosConsulta);
 })
 
