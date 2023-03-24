37c8f7934385961d3cad1a92de0fa8324ae5fafd
mtavidal <119874263+mtavidal@users.noreply.github.com>
2023-03-24 16:44:25 -0300

    adiciona css

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ buscarMarcaPorTipo.onchange = async ()  => {
     let tipo = buscarMarcaPorTipo.value;
     let marcas = await listarMarcas(tipo);
     let selectMarca = document.getElementById("marca");
-    selectMarca.style.display = "flex";
+    selectMarca.disabled = false;
     selectMarca.innerHTML = `<option value="" disabled selected>selecione</option>`;
     for (let index = 0; index < marcas.length; index++) {
         selectMarca.innerHTML += ` 
@@ -22,7 +22,7 @@ buscarModeloPorMarca.onchange = async ()  => {
     let marca = buscarModeloPorMarca.value;
     let modelos = await listarModelos(tipo, marca);
     let selectModelo = document.getElementById("modelo");
-    selectModelo.style.display = "flex";
+    selectModelo.disabled = false;
     selectModelo.innerHTML = `<option value="" disabled selected>selecione</option>`;
     for (let index = 0; index < modelos.modelos.length; index++) {
         selectModelo.innerHTML += ` 
@@ -38,7 +38,7 @@ buscarAnoPorModelo.onchange = async ()  => {
     let modelo = buscarAnoPorModelo.value;
     let anos = await listarAnos(tipo, marca, modelo);
     let selectAno = document.getElementById("ano");
-    selectAno.style.display = "flex";
+    selectAno.disabled = false;
     selectAno.innerHTML = `<option value="" disabled selected>selecione</option>`;
     for (let index = 0; index < anos.length; index++) {
         selectAno.innerHTML += ` 
@@ -56,5 +56,15 @@ form.addEventListener('submit', async event => {
     let ano = buscarCarroPorAno.value;
     let dadosConsulta = await consultarCarro(tipo, marca, modelo, ano);
     console.log(dadosConsulta);
+    let divDadosCarro = document.getElementById("dadosCarro");
+    divDadosCarro.innerHTML = `
+        <div id = "card">
+            <h1>Marca: ${dadosConsulta.Marca}</h1>
+            <h1>Modelo: ${dadosConsulta.Modelo}</h1>
+            <h1>Ano: ${ano}</h1>
+            <h1>Valor: ${dadosConsulta.Valor}</h1>
+            <h4>Mês de referência: ${dadosConsulta.MesReferencia}</h4>
+        </div>
+    `
 })
 
